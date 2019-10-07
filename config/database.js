26ac9cb3d0694e79bb0a3b1a3f9546a2f5d48696
Pillippa Perez <ignacia.pp@gmail.com>
2019-10-07 19:47:05 -0300

    add database configuration and pino logger

---
+++ config/database.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const DatabaseConnection = logger => {
+  const { DATABASE_URL } = process.env;
+  mongoose.connect(DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+  mongoose.connection.on("connected", () => {
+    logger.info(`Mongoose default connection is open to ${DATABASE_URL}`);
+  });
+
+  mongoose.connection.on("error", err => {
+    logger.info(`Mongoose default connection has occured ${err} error`);
+  });
+
+  mongoose.connection.on("disconnected", () => {
+    logger.info(`Mongoose default connection is disconnected`);
+  });
+
+  process.on("SIGINT", () => {
+    mongoose.connection.close(() => {
+      logger.info(
+        `Mongoose default connection is disconnected due to application termination`
+      );
+      process.exit(0);
+    });
+  });
+};
+
+module.exports = DatabaseConnection;
